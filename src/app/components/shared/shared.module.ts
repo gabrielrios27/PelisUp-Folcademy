514b5dfc29be30bf0c9507e20fb4d58688068188
gabrielrios27 <juangrios27@gmail.com>
2022-03-21 12:52:57 -0300

    se agrego un filtro ngIf en home, peliculas, series y addNewItem para que no se muestren cards con una imagen nula en poster_path - se corrigio responsive de los titulos H1

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearcherComponent } from './searcher/searcher.component';
-import { CardsConteinerComponent } from './cards-conteiner/cards-conteiner.component';
+
 import { CardComponent } from './card/card.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [SearcherComponent, CardsConteinerComponent, CardComponent],
+  declarations: [SearcherComponent, CardComponent],
   imports: [CommonModule, RouterModule, FormsModule],
-  exports: [
-    CommonModule,
-    SearcherComponent,
-    CardComponent,
-    CardsConteinerComponent,
-    RouterModule,
-  ],
+  exports: [CommonModule, SearcherComponent, CardComponent, RouterModule],
 })
 export class SharedModule {}
